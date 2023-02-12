f70812b39b10a373a7ddafffeb4ad39d69a90856
EL LAZ Mohamad <mohamad.el-laz@inria.fr>
2023-02-12 13:01:05 +0100

    Define the group and keys type

--- src/elgamal.ml
+++ src/elgamal.ml
@@ -1,1 +1,27 @@
 (* Plain implementation of ElGamal scheme *)
+
+
+(* Define the type of group and keys *)
+
+type group = {
+  pbits : int;
+  p     : Z.t;
+  g     : Z.t;
+}
+
+type key = {
+  group : group;
+  key   : Z.t;
+             }
+
+type pkey = key
+type skey = key
+
+type keys = {
+  skey : skey;
+  pkey : pkey;
+}
+
+type cipher = Z.t * Z.t
+
+(* --------------------------------------------------------------------------- *)
