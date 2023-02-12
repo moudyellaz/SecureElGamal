7bf9a405dff329692e81429be7ee5f79bd2ba8d5
EL LAZ Mohamad <mohamad.el-laz@inria.fr>
2023-02-12 13:04:36 +0100

    Add some functions

--- src/elgamal.ml
+++ src/elgamal.ml
@@ -25,3 +25,14 @@ type keys = {
 type cipher = Z.t * Z.t
 
 (* --------------------------------------------------------------------------- *)
+
+(* [q p] returns [p/2] *)
+let q p = Z.shift_right p 1
+
+(* [quad p q x] tests if x is a quadratic residue in p *)
+let quad p x = Z.equal ( Z.powm x (q p) p) Z.one 
+
+(* [ mulm gr m n] returns the multiplication modulo p *)
+let mulm gr m n = Z.(mod) (Z.mul m n) gr.p
+
+(* --------------------------------------------------------------------------- *)
