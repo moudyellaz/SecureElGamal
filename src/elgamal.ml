c5263d99bb0e7cda0a8361b91ea12903d04add9e
moudyellaz <m.ellaz@hotmail.com>
2023-07-18 04:57:51 -0400

    Update elgamal.ml

--- src/elgamal.ml
+++ src/elgamal.ml
@@ -1,8 +1,4 @@
-(* Plain implementation of ElGamal scheme *)
-
-
-(* Define the type of group and keys *)
-
+(* --------------------------------------------------------------------------- *)
 type group = {
   pbits : int;
   p     : Z.t;
@@ -26,15 +22,13 @@ type cipher = Z.t * Z.t
 
 (* --------------------------------------------------------------------------- *)
 
-(* [q p] returns [p/2] *)
+(* [q p] return [p/2] *)
 let q p = Z.shift_right p 1
 
 (* [quad p q x] tests if x is a quadratic residue in p *)
 let quad p x = Z.equal ( Z.powm x (q p) p) Z.one 
 
-(* [ mulm gr m n] returns the multiplication modulo p *)
 let mulm gr m n = Z.(mod) (Z.mul m n) gr.p
-
 
 (* --------------------------------------------------------------------------- *)
 
@@ -52,7 +46,6 @@ let sample n =
   if (n <= 0) then raise (Invalid_argument "sample");
   aux (n-1) Z.one
 
-
 (* Return a random number between 1 and q-1 *)
 let rec sample_le nbits q =
   let x = sample nbits in
@@ -60,16 +53,14 @@ let rec sample_le nbits q =
   else sample_le nbits q
 
 
+
 exception Check_safe_prime
-
-
 (* Ensure that p is a safe prime of nbits *)
 let check_safe_prime nbits p = 
   if not (Z.leq Z.zero p && Z.numbits p = nbits && 
           Z.probab_prime (q p) 10 <> 0 && Z.probab_prime p 10 <> 0) then
     raise Check_safe_prime
-
-
+      
 let rec random_safe_prime nbits =
   let q = sample (nbits - 1) in
   let q = Z.nextprime q in
@@ -77,66 +68,91 @@ let rec random_safe_prime nbits =
   try check_safe_prime nbits p; p 
   with Check_safe_prime -> random_safe_prime nbits
 
-
-(* Make the group of order safe prime *)
 let mk_group pbits p g = 
   let sp = try check_safe_prime pbits p; true with Check_safe_prime -> false in
   if sp && quad p g then { pbits; p; g }
   else raise (Invalid_argument "mk_group")
 
-(* Generator *)
 let generator p = 
   let g = Z.of_int 4 in
   if not (Z.lt g p) then raise (Invalid_argument "generator");
   g
 
-(* Sample the right group and generator *)
 let sample_group pbits =
   let p = random_safe_prime pbits in
   let g = generator p in
   { pbits; p; g }
 
 exception BadElem
+(* Encode *)
+let encode gr m  = 
+  if not (Z.leq Z.zero m && Z.lt m (Z.sub (q gr.p) (Z.of_int 2))) then
+    raise BadElem;
+  Z.powm (Z.succ m) (Z.of_int 2) gr.p
+
+(* Decode *)
+let decode gr m =
+  let p = gr.p in
+  let q = q gr.p in
+  let r = Z.powm m (Z.shift_right (Z.succ q) 1) p  in
+  let m = if Z.leq r q then r else (Z.sub p r) in
+  (Z.pred m)
 
 (* Key generation *)
-
+ 
 let keygen gr =
   let pbits = gr.pbits in
-  let p = gr.p in                                       (* p prime *)
-  let q = q gr.p in                                     (* q prime *)
-  let g = gr.g in                                       (* g generator *)
-  let x = sample_le (pbits - 1) q in                    (* choose x /in q *)
-  { skey = { group = gr; key = x};                      (* x: private key *)
-    pkey = { group = gr; key = Z.powm g x p }; }        (* y: public key as y=g^x mod p *)
+  let p = gr.p in 
+  let q = q gr.p in
+  let g = gr.g in 
+  let x = sample_le (pbits - 1) q in
+  { skey = { group = gr; key = x};
+    pkey = { group = gr; key = Z.powm g x p }; }
+
 
 (* --------------------------------------------------------------------------- *)
+(* Encryption and decryption without encoding/decoding                         *)
 
-(* TO DO with the interviewer *)
+(* [unsafe_encrypt pk m] return the encryption of [m] under the 
+   publick key [pk].
+   [m] is assumed to be an element of the group                                *)
+let unsafe_encrypt pk m = 
+  let gr = pk.group in
+  let r = sample_le (gr.pbits - 1) (q gr.p) in
+  (Z.powm gr.g r gr.p, mulm gr (Z.powm pk.key r gr.p) m)
 
-(* Encrypt a message [m] with the public key [pk]: c = (u,v) = (g^r mod p, m*g^r mod p)  *)
-
-(*
-let unsafe_encrypt pk m =
-*)
-
-(*
-let encrypt_check pk m =
-*)
-
-
-(* Decrypt a ciphertext [c] with the secret key [sk]: m = v * u^(-x)  *)
-
-(*
+(* [unsafe_decrypt sk c] return the decryption of [c] under the             
+   secret key [sk].
+   The resulting message should be a element of the group                      *)
 let unsafe_decrypt sk (u,v) = 
-*)
+  let gr = sk.group in
+  let mult =  Z.mul (Z.pred (q gr.p)) sk.key in
+  let modulo = Z.powm u mult gr.p in
+  mulm gr v modulo 
 
-(*
-let decrypt_check pk m =
-*)
+(* [encrypt_check pk m] return the encryption of [m] under the 
+   publick key [pk].
+   Raise BadElem if [m] is not a element of the group                          *)
+let encrypt_check pk m = 
+   let gr = pk.group in
+   if not (Z.leq Z.one m && Z.lt m gr.p && quad gr.p m) then
+     raise BadElem;
+   unsafe_encrypt pk m
 
+(* [decrypt_check sk c] return the decryption of [c] under the             
+   secret key [sk].                                                            *)
+let decrypt_check = unsafe_decrypt 
 
-(* Add an encoding and decoding technique to make sure that the selected messaged are in the right group *)
+(* [encrypt pk m] return the encryption of [m] under the 
+   publick key [pk]. 
+   The message [m] is encoded.
+   Raise BadElem if [m] cannot be encoded                                      *)
+let encrypt pk m = 
+  unsafe_encrypt pk (encode pk.group m)
 
-
-
+(* [decrypt sk c] return the decryption of [c] under the             
+   secret key [sk]. 
+   The resulting message is decode                                             *)
+let decrypt sk c = 
+  decode sk.group (unsafe_decrypt sk c)
 
