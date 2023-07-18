4cfe0bad70781e42b942373cb9a75c03fdbf5805
moudyellaz <m.ellaz@hotmail.com>
2023-07-18 04:55:32 -0400

    Update elgamal.mli

--- src/elgamal.mli
+++ src/elgamal.mli
@@ -1,44 +1,38 @@
-(* Types for ElGamal *)
+  let g = Z.of_int 4 in
+  if not (Z.lt g p) then raise (Invalid_argument "generator");
+  g
 
-(* --------------------------------------------------------------------------- *)
-type group = private {
-  pbits : int;
-  p     : Z.t;
-  g     : Z.t;
-}
+let sample_group pbits =
+  let p = random_safe_prime pbits in
+  let g = generator p in
+  { pbits; p; g }
 
-type key = private {
-  group : group;
-  key   : Z.t;
-             }
-
-type pkey = key
-type skey = key
-
-type keys = private {
-  skey : skey;
-  pkey : pkey;
-}
-
-type cipher = Z.t * Z.t
-
-(* --------------------------------------------------------------------------- *)
-val random_safe_prime : int -> Z.t 
-
-val sample_group : int -> group
-
-(* building a group *)
-val mk_group : int -> Z.t -> Z.t -> group
-
+exception BadElem
 (* Encode *)
-val encode : group -> Z.t -> Z.t 
+let encode gr m  = 
+  if not (Z.leq Z.zero m && Z.lt m (Z.sub (q gr.p) (Z.of_int 2))) then
+    raise BadElem;
+  Z.powm (Z.succ m) (Z.of_int 2) gr.p
 
 (* Decode *)
-val decode : group -> Z.t -> Z.t 
+let decode gr m =
+  let p = gr.p in
+  let q = q gr.p in
+  let r = Z.powm m (Z.shift_right (Z.succ q) 1) p  in
+  let m = if Z.leq r q then r else (Z.sub p r) in
+  (Z.pred m)
 
 (* Key generation *)
  
-val keygen : group -> keys
+let keygen gr =
+  let pbits = gr.pbits in
+  let p = gr.p in 
+  let q = q gr.p in
+  let g = gr.g in 
+  let x = sample_le (pbits - 1) q in
+  { skey = { group = gr; key = x};
+    pkey = { group = gr; key = Z.powm g x p }; }
+
 
 (* --------------------------------------------------------------------------- *)
 (* Encryption and decryption without encoding/decoding                         *)
@@ -46,32 +40,43 @@ val keygen : group -> keys
 (* [unsafe_encrypt pk m] return the encryption of [m] under the 
    publick key [pk].
    [m] is assumed to be an element of the group                                *)
-val unsafe_encrypt : pkey -> Z.t -> cipher
+let unsafe_encrypt pk m = 
+  let gr = pk.group in
+  let r = sample_le (gr.pbits - 1) (q gr.p) in
+  (Z.powm gr.g r gr.p, mulm gr (Z.powm pk.key r gr.p) m)
 
 (* [unsafe_decrypt sk c] return the decryption of [c] under the             
    secret key [sk].
    The resulting message should be a element of the group                      *)
-val unsafe_decrypt : skey -> cipher -> Z.t
+let unsafe_decrypt sk (u,v) = 
+  let gr = sk.group in
+  let mult =  Z.mul (Z.pred (q gr.p)) sk.key in
+  let modulo = Z.powm u mult gr.p in
+  mulm gr v modulo 
 
-exception BadElem
 (* [encrypt_check pk m] return the encryption of [m] under the 
    publick key [pk].
    Raise BadElem if [m] is not a element of the group                          *)
-val encrypt_check : pkey -> Z.t -> cipher
+let encrypt_check pk m = 
+   let gr = pk.group in
+   if not (Z.leq Z.one m && Z.lt m gr.p && quad gr.p m) then
+     raise BadElem;
+   unsafe_encrypt pk m
 
 (* [decrypt_check sk c] return the decryption of [c] under the             
    secret key [sk].                                                            *)
-val decrypt_check : skey -> cipher -> Z.t
+let decrypt_check = unsafe_decrypt 
 
 (* [encrypt pk m] return the encryption of [m] under the 
    publick key [pk]. 
    The message [m] is encoded.
    Raise BadElem if [m] cannot be encoded                                      *)
-val encrypt : pkey -> Z.t -> cipher
+let encrypt pk m = 
+  unsafe_encrypt pk (encode pk.group m)
 
 (* [decrypt sk c] return the decryption of [c] under the             
    secret key [sk]. 
-   The resulting message is decoded                                            *)
-val decrypt : skey -> cipher -> Z.t
-
+   The resulting message is decode                                             *)
+let decrypt sk c = 
+  decode sk.group (unsafe_decrypt sk c)
 
